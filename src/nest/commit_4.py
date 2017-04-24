f6a497bab45323f90abc4054ba7861face849745
mulala <mulala00@gmail.com>
2017-04-24 14:24:40 +0800

    Iterator Edit Every File

--- src/nest/commit_4.py
+++ src/nest/commit_4.py
@@ -598,3 +598,4 @@ ssasa
 [2017-04-24-13:36:09] Edit by 'iter_edit_files'
 [2017-04-24-13:54:35] Edit by 'iter_edit_files'
 [2017-04-24-14:16:25] Edit by 'iter_edit_files'
+[2017-04-24-14:24:40] Edit by 'iter_edit_files'

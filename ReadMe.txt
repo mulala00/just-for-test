54c0a8d083062a96910d826e92a4416b75f1a9b6
mulala <mulala00@gmail.com>
2017-04-27 21:09:04 +0800

    Iterator Edit Every File

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,4 @@
 
 [2017-04-27-10:15:41] Edit by 'iter_edit_files'
 [2017-04-27-20:04:44] Edit by 'iter_edit_files'
+[2017-04-27-21:09:01] Edit by 'iter_edit_files'
--- src/nest/commit_4.py
+++ src/nest/commit_4.py
@@ -617,3 +617,4 @@ ssasa
 [2017-04-27-09:55:18] Edit by 'iter_edit_files'
 [2017-04-27-10:15:41] Edit by 'iter_edit_files'
 [2017-04-27-20:04:44] Edit by 'iter_edit_files'
+[2017-04-27-21:09:02] Edit by 'iter_edit_files'

6bb32ca35fad5068d40ef51200963c1fe22967e4
mulala <mulala00@gmail.com>
2017-08-17 19:59:06 +0800

    Iterator Edit Every File

--- src/nest/commit_7.py
+++ src/nest/commit_7.py
@@ -669,3 +669,4 @@
 [2017-08-14-20:00:54] Edit by 'iter_edit_files'
 [2017-08-15-20:00:56] Edit by 'iter_edit_files'
 [2017-08-16-19:58:48] Edit by 'iter_edit_files'
+[2017-08-17-19:59:06] Edit by 'iter_edit_files'
